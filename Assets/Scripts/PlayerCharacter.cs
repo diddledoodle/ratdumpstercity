e43db7a207bdb17682ea4367f76f01ea76ffb78d
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2018-09-25 15:19:38 -0500

    changed speed added getaxis removed getkey

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -3,22 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
-    public Rigidbody2D myRigidbody;
-	// Use this for initialization
-	void Start () {
 
-        Debug.Log("This is start");
+    [SerializeField]
+    private float speed = 5;
+
+    [SerializeField]
+    private Rigidbody2D myRigidbody;
+
+    [SerializeField]
+    private float size = 5, strength = 10, height, weight;
+
+    private float moveInput;
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log("This is the start");
 	}
-	
+ 
+
 	// Update is called once per frame
-	void Update () {
-
-        if (Input.GetKey(KeyCode.D))
+	void Update () 
         {
-            // TO DO: move the character to the right!
-            myRigidbody.velocity = new Vector2(5, myRigidbody.velocity.y);
+            GetMovementInput();
         }
-      // This is the syntax for printing to the console!
-      //  Debug.Log("Test");
-	}
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetMovementInput()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+    }
+
+    private void Move()
+    {
+        //Dont use transform.Translate because we are using physics!
+        // transform.Translate(speed, 0, 0);
+        myRigidbody.velocity = new Vector2(moveInput * speed, myRigidbody.velocity.y);
+    }
+
+    private void Jump()
+    {
+        //TODO make the character jump!
+    }
+
+    // This is the syntax for printing to the console!
+    //  Debug.Log("Test");
 }
+
