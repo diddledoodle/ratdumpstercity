80cfd5fdb78974d0a4f9b630b9eea293d50a46d7
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2018-10-09 15:23:14 -0500

    physics changed checkpoint added

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed = 100;
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+    private void UpdateRotation()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player entering the checkpoint.");
+            PhysicsPlayerCharacter player = collision.GetComponent<PhysicsPlayerCharacter>();
+            player.SetCurrentCheckpoint(this);
+
+        }
+
+    }
+}
--- Assets/Scripts/PhysicsPlayerCharacter.cs
+++ Assets/Scripts/PhysicsPlayerCharacter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PhysicsPlayerCharacter : MonoBehaviour {
 
@@ -17,6 +18,12 @@ public class PhysicsPlayerCharacter : MonoBehaviour {
     private Rigidbody2D rb2d;
 
     [SerializeField]
+    private Collider2D playerGroundCollider;
+
+    [SerializeField]
+    private PhysicsMaterial2D playerMovingPhysicsMaterial, playerStoppingPhysicsMaterial;
+
+    [SerializeField]
     private ContactFilter2D groundContactFilter;
 
     [SerializeField]
@@ -25,6 +32,7 @@ public class PhysicsPlayerCharacter : MonoBehaviour {
     private float horizontalInput;
     private bool isOnGround;
     private Collider2D[] groundHitDetectionResults = new Collider2D[16];
+    private Checkpoint currentCheckpoint;
 	
 	// Update is called once per frame
 	void Update ()
@@ -35,6 +43,25 @@ public class PhysicsPlayerCharacter : MonoBehaviour {
         HandleJumpInput();
 
     }
+
+    private void FixedUpdate()
+    {
+        UpdatePhysicsMaterial();
+        Move();
+
+    }
+
+    private void UpdatePhysicsMaterial()
+    {
+        if (Mathf.Abs(horizontalInput) > 0)
+        {
+            playerGroundCollider.sharedMaterial = playerMovingPhysicsMaterial;
+        }
+        else
+        {
+            playerGroundCollider.sharedMaterial = playerStoppingPhysicsMaterial;
+        }
+    }
     private void UpdateIsOnGround()
     {
         isOnGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
@@ -44,7 +71,7 @@ public class PhysicsPlayerCharacter : MonoBehaviour {
 
     private void UpdateHorizontalInput()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
     }
 
     private void HandleJumpInput()
@@ -55,17 +82,29 @@ public class PhysicsPlayerCharacter : MonoBehaviour {
         }
     }
 
-    private void FixedUpdate()
-    {
-        Move();
-
-    }
-
     private void Move()
     {
         rb2d.AddForce(Vector2.right * horizontalInput * accelerationForce);
         Vector2 clampedVelocity = rb2d.velocity;
         clampedVelocity.x = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
         rb2d.velocity = clampedVelocity;
+    }
+
+    public void Respawn()
+    {
+        if (currentCheckpoint == null)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+            transform.position = currentCheckpoint.transform.position;
+        }
+        
+    }
+
+    public void SetCurrentCheckpoint(Checkpoint newCurrentCheckpoint)
+    {
+        currentCheckpoint = newCurrentCheckpoint;
     }
 }
