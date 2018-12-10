ab95a078fcbdb1451ccddd2c05a9ca98332b935e
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2018-12-09 22:46:07 -0600

    added scripts into code review branch

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour
+{
+    PhysicsPlayerCharacter player;
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.GetComponent<PhysicsPlayerCharacter>();
+
+            Debug.Log("Player entered the hazard.");
+          
+            
+
+
+            player.canDoInput = false;
+            player.GetComponent<Rigidbody2D>().velocity = new Vector3 (0,0,0);
+            player.transform.localScale = new Vector3(player.transform.localScale.x, -1, player.transform.localScale.z);
+            
+
+            StartCoroutine(pauseenumerator());
+          
+
+            
+
+        }
+        else
+        {
+            Debug.Log("Something other than the player entered the hazard.");
+        }
+        
+    }
+
+    IEnumerator pauseenumerator()
+    {
+
+        yield return new WaitForSeconds(0.7f);
+
+        player.Respawn();
+ 
+    }
+
+}
