fb998bab273fb05d4159fa03dd420e7e18e9afad
diddledoodle <43079367+diddledoodle@users.noreply.github.com>
2018-12-17 22:29:38 -0600

    added quit script added quit button finalized title screen

---
+++ Assets/Scripts/LoadScreenScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadScreenScript : MonoBehaviour {
+
+    [SerializeField]
+    private string scenetoLoad;
+
+    public void LoadByScene(string scenetoLoad)
+    {
+        SceneManager.LoadScene(scenetoLoad);
+    }
+}
+
+
